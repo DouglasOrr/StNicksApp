a66756f34439b6a4565d4f497a4aef7a9397426f
Douglas Orr <douglas.orr99@gmail.com>
2018-03-17 15:14:52 +0000

    Code cleanup & fix action bar background

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # St Nicks App
 
 An app for church members at St Nick's Church.
+
+
+## Storage account
+
+ - Name: `stnicksapp`
+ - Model: `Resource manager`
+ - Account kind: `Blob storage`
+ - Performance: `Standard`
+ - Access tier: `Hot`
+ - Replication: `LRS`
+ - Secure transfer required: `Disabled`
+ - Resource group `stnicksapp`
+ - Location: `West Europe`
--- app/app/src/main/java/uk/org/stnickschurch/stnicksapp/Home.java
+++ app/app/src/main/java/uk/org/stnickschurch/stnicksapp/Home.java
@@ -1,5 +1,6 @@
 package uk.org.stnickschurch.stnicksapp;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,101 +20,77 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class Home extends AppCompatActivity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    @BindView(R.id.toolbar) Toolbar mToolbar;
+    @BindView(R.id.container) ViewPager mViewPager;
+    @BindView(R.id.tabs) TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
+        setSupportActionBar(mToolbar);
+        mViewPager.setAdapter(new SectionsPagerAdapter(getSupportFragmentManager()));
+        mTabLayout.setupWithViewPager(mViewPager);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            // TODO: go to settings
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     public static class MediaFragment extends Fragment {
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(R.string.section_media);
+            rootView.<TextView>findViewById(R.id.section_label).setText(R.string.section_media);
             return rootView;
         }
     }
 
     public static class CalendarFragment extends Fragment {
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(R.string.section_calendar);
+            ((TextView) rootView.findViewById(R.id.section_label)).setText(R.string.section_calendar);
             return rootView;
         }
     }
 
     public static class PrayerFragment extends Fragment {
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(R.string.section_prayer);
+            ((TextView) rootView.findViewById(R.id.section_label)).setText(R.string.section_prayer);
             return rootView;
         }
     }
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
+        SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
